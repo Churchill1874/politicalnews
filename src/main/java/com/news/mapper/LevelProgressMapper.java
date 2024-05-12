4a61b71a7a5591a4af18e937cfdd0bbe55ca5013
churchill <asdf@@123>
2024-05-13 01:57:47 +0900

    1.0

--- src/main/java/com/news/mapper/LevelProgressMapper.java
+++ src/main/java/com/news/mapper/LevelProgressMapper.java
@@ -6,12 +6,15 @@ import org.apache.ibatis.annotations.Update;
 
 public interface LevelProgressMapper extends BaseMapper<LevelProgress> {
 
+    //增加评论次数
     @Update("UPDATE level_progress SET comment_count = comment_count + 1 WHERE account = #{account}")
     void addCommentCount(Integer account);
 
+    //增加被赞次数
     @Update("UPDATE level_progress SET likes_received_count = likes_received_count + 1 WHERE account = #{account}")
     void addLikesReceivedCount(Integer account);
 
+    //增加下注正确次数
     @Update("UPDATE level_progress SET correct_count = correct_count + 1 WHERE account = #{account}")
     void addCorrectCount(Integer account);
 
